8fac265ea765d8f7b75a370cf9e47d6ab8fbb82f
Cebrian10 <aldaircebrian@hotmail.com>
2023-09-06 08:24:11 -0500

    Creando Index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: IndexComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
